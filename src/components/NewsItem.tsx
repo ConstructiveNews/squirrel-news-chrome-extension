3ab33a28753a4389ef969bf6a3cc7feb1c7dbbaf
ikorotkaya <irinkakorotkaya@gmail.com>
2023-12-25 15:16:37 +0100

    Show issue images

--- src/components/NewsItem.tsx
+++ src/components/NewsItem.tsx
@@ -11,8 +11,9 @@ export default function NewsItem({ issue }: NewsItemProps) {
       href={issue.issueURL}
       target="_blank"
       className="flex flex-col bg-slate-300 p-12 rounded-md hover:bg-slate-200">
-      <div>{issue.headline}</div>      
-      <div>{moment(issue.publishedAt).format('MMMM Do YYYY')}</div>
+      <h2 className="text-xl font-bold mb-4">{issue.headline}</h2>      
+      <div className="mb-2">{moment(issue.publishedAt).format('MMMM Do YYYY')}</div>
+      <img className="mb-4" src={issue.image} alt={issue.headline} />
       <div>{issue.teaser}</div>
     </a>
   );
