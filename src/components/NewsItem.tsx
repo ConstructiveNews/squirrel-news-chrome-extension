cc8458c87b38b6c4ef7d70c7f7d5cfd4e1195ab9
ikorotkaya <irinkakorotkaya@gmail.com>
2023-12-26 16:16:39 +0100

    Change h3 text size

--- src/components/NewsItem.tsx
+++ src/components/NewsItem.tsx
@@ -13,7 +13,7 @@ export default function NewsItem({ issue }: NewsItemProps) {
       className="flex flex-col">
       <img className="mb-4" src={issue.image} alt={issue.headline} />
       <div className="mb-1 text-sm">{moment(issue.publishedAt).format('MMMM Do YYYY')}</div>
-      <h3 className="text-md font-bold mb-4">{issue.headline}</h3>      
+      <h3 className="text-lg font-bold mb-4">{issue.headline}</h3>      
       {/* <div>{issue.teaser}</div> */}
     </a>
   );
